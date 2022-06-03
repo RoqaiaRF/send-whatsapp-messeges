cfae919708e9c74e959a7d83b7ae75c414f96155
Roqaia Alrfou <85272476+RoqaiaRF@users.noreply.github.com>
2022-06-03 22:31:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const sql = require("mssql");//25/12/2021
+
 
 
 app.use(express.json());
